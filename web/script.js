3de6b1c394ac032f4e8eb462244e632990993cbf
Elmnanmu <1842059836@qq.com>
2025-02-22 17:21:01 +0800

    Delete web directory

--- web/script.js
+++
@@ -1,7 +0,0 @@
-// 为导航链接添加点击事件
-document.querySelectorAll('nav a').forEach(link => {
-    link.addEventListener('click', (event) => {
-        event.preventDefault(); // 阻止默认跳转
-        alert('你点击了：' + link.textContent);
-    });
-});
